481193165862eaec736aa81478394e0ef579315b
wufei <wufei1228@hotmail.com>
2024-06-23 17:59:57 +0800

    build

---
+++ C/build/CMakeFiles/mfcc.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/AAA/VESC/speech-dectection/Speech-detection/C/Src/main.c" "CMakeFiles/mfcc.dir/Src/main.obj" "gcc" "CMakeFiles/mfcc.dir/Src/main.obj.d"
+  "E:/AAA/VESC/speech-dectection/Speech-detection/C/Src/mfcc.c" "CMakeFiles/mfcc.dir/Src/mfcc.obj" "gcc" "CMakeFiles/mfcc.dir/Src/mfcc.obj.d"
+  "E:/AAA/VESC/speech-dectection/Speech-detection/C/Src/preproc.c" "CMakeFiles/mfcc.dir/Src/preproc.obj" "gcc" "CMakeFiles/mfcc.dir/Src/preproc.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ C/build/CMakeFiles/mfcc.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mfcc.dir/Src/main.obj"
+  "CMakeFiles/mfcc.dir/Src/main.obj.d"
+  "CMakeFiles/mfcc.dir/Src/mfcc.obj"
+  "CMakeFiles/mfcc.dir/Src/mfcc.obj.d"
+  "CMakeFiles/mfcc.dir/Src/preproc.obj"
+  "CMakeFiles/mfcc.dir/Src/preproc.obj.d"
+  "libmfcc.dll.a"
+  "mfcc.exe"
+  "mfcc.exe.manifest"
+  "mfcc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mfcc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
